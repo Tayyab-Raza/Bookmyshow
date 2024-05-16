f534f6bf008c34c26973a715f911472379a40672
Tayyab-Raza <traza9192@gmail.com>
2024-05-16 18:06:47 +0530

    work

--- bookmyshow/src/Components/HeroCarousel/HeroCarousel.Component.jsx
+++ bookmyshow/src/Components/HeroCarousel/HeroCarousel.Component.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import HeroSlider from 'react-slick'
+
 
 const HeroCarousel = () => {
+    const [images, setImages] = useState([]);
+    var settingsLg = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
   return (
-    <div>
-      HeroCarousel
+    <>
+    <div className='lg:hidden'>
+     <HeroSlider {...settings}>
+        {
+            images.map((image) => (
+                <div className='w-full h-full md:h-80 py-3'>
+                    <img src="https://image.tmdb.org/t/p/original/ugS5FVfCI3RV0ZwZtBV3HAV75OX.jpg" alt="Hero Banner" className='w-full h-full rounded-md object-cover' />
+                </div>
+            ))
+        }
+     </HeroSlider>
     </div>
+    <div className='hidden lg:block'>
+    <HeroSlider {...settingsLg}>
+        {
+            images.map((image) => (
+                <div className='w-full h-full md:h-80 py-3'>
+                    <img src="https://image.tmdb.org/t/p/original/ugS5FVfCI3RV0ZwZtBV3HAV75OX.jpg" alt="Hero Banner" className='w-full h-full rounded-md object-cover' />
+                </div>
+            ))
+        }
+     </HeroSlider>
+    </div>
+    </>
   )
 }
 
--- bookmyshow/src/pages/Home.Page.jsx
+++ bookmyshow/src/pages/Home.Page.jsx
@@ -4,8 +4,8 @@ import Defaultlayout from '../Layouts/Default.layout'
 
 // Components
 import EntertainmentCardSlider from '../Components/Entertainment/EntertainmentCard.Component'
-import HeroCarousel from '../Components/HeroCarousel/HeroCarousel.Component'
 import PosterSlider from '../Components/PosterSlider/PosterSlider.Component'
+import HeroCarousel from '../Components/HeroCarousel/HeroCarousel.Component'
 
 
 const HomePage = () => {
@@ -17,6 +17,41 @@ const HomePage = () => {
   return (
     <>
     <HeroCarousel />
+    <div className='contaner mx-auto px-4 md:px-12 my-8'>
+    <h1 className='text-2xl font-bold text-gray-800 sm:ml-3 my-3'>The best of Entertainment</h1>
+    <EntertainmentCardSlider />
+    </div>
+
+    <div className='contaner mx-auto px-4 md:px-12 my-8'>
+    <PosterSlider 
+    title = "Recommended Movies"
+    poster = {recommendedMovie}
+    isDark = {false}
+    />
+    </div>
+
+    <div className='bg-premier-800 py-12'>
+      <div className='contaner mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+        <div className='hidden md:flex'>
+          <img src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-banner-web-collection-202208191200.png" alt="Rupay" className='w-full h-full' />
+        </div>
+        <PosterSlider 
+    title = "Premieres"
+    subtitle = "Brand New releases every friday"
+    poster = {premierMovies}
+    isDark = {true}
+    />
+      </div>
+    </div>
+   
+   <div className='contaner mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+   <PosterSlider 
+    title = "Online Streaming Events"
+    subtitle = "Brand New online streaming events"
+    poster = {onlineStreamEvents}
+    isDark = {false}
+    />
+   </div>
     </>
   )
 }
